0a99dab8444d26a473ea6786bb03f801d7cec47d
Muhammad Faisal <muh.faisal572@gmail.com>
2023-10-16 16:51:48 +0700

    fix bug

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -101,9 +101,11 @@ export function NavigationBar() {
           </Typography>
           <div className="mr-4 hidden lg:block">{navList}</div>
           <div className="flex items-center gap-4">
-            <Button size="sm" className="hidden lg:inline-block bg-blue-600">
-              <Link href="/auth/register">Sign Up</Link>
-            </Button>
+            <Link href="/auth/register">
+              <Button size="sm" className="hidden lg:inline-block bg-blue-600">
+                Sign Up
+              </Button>
+            </Link>
             <IconButton
               variant="text"
               className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
